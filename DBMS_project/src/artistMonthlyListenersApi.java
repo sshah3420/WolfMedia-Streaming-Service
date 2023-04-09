b88cf7bbacc812bae32db92665fb0f435c389eda
Shaival Shah <31449664+sshah3420@users.noreply.github.com>
2023-04-09 17:20:25 -0400

    artistMonthlyListenersApi completed

---
+++ DBMS_project/src/artistMonthlyListenersApi.java
@@ -0,0 +1,123 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Homework2 {
+	
+	private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/sshah34";
+	private static final String user = "sshah34";
+	private static final String password = "200474707";
+
+	public static Connection connection = null;
+	public static Statement stmt = null;
+	public static PreparedStatement ps = null;
+	public static ResultSet rs = null;
+	
+	public static void enterArtistMonthlyListeners(int artist_id, int count_id) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			// Get Connection object.
+			connection = DriverManager.getConnection(jdbcURL, user, password);
+			System.out.println(connection);
+
+			// Query to insert the record into ARTICLES table.
+			String insertSql = "INSERT INTO Monthly_artist_count(artist_id, count) VALUES (?, ?)";
+			// Create PreparedStatement Object for given insert query.
+			ps = connection.prepareStatement(insertSql);
+			// Set values with user-passed arguments.
+			ps.setInt(1, artist_id);
+			ps.setInt(2, count_id);
+//			ps.setString(3, artName);
+//			ps.setString(4, artText);
+//			ps.setString(5, artCreationDate);
+			ps.executeUpdate();
+			System.out.println("Monthly Artist Count Added.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Close PreparedStatement and Connection Objects.
+			close(ps);
+			close(connection);
+		}
+	}
+	public static void updateArtistMonthlyListeners(int artist_id, int count_no) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			// Get Connection object.
+			connection = DriverManager.getConnection(jdbcURL, user, password);
+			System.out.println(connection);
+
+			// Query to insert the record into ARTICLES table.
+			String insertSql = " UPDATE Monthly_artist_count SET count = ? WHERE artist_id = " + artist_id;
+			// Create PreparedStatement Object for given insert query.
+			ps = connection.prepareStatement(insertSql);
+			// Set values with user-passed arguments.
+			ps.setInt(1, count_no);
+//			ps.setInt(2, count_id);
+//			ps.setString(3, artName);
+//			ps.setString(4, artText);
+//			ps.setString(5, artCreationDate);
+			ps.executeUpdate();
+			System.out.println("Monthly Artist Count Updated.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Close PreparedStatement and Connection Objects.
+			close(ps);
+			close(connection);
+		}
+	}
+	// method to close Connection.
+	static void close(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	// method to Statement.
+	static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	// method to close PreparedStatement.
+	static void close(PreparedStatement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	// method to close ResultSet
+	static void close(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+		// int roleChoice = -1;
+		System.out.println("Hello");
+//		Homework2.enterArtistMonthlyListeners(2001, -99);
+//		Homework2.updateArtistMonthlyListeners(2001, -100);
+		
+	}
+
+
+}
