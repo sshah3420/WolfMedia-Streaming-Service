738e18b93d1a01fe8eb19266ac71c257df13acce
Shaival Shah <31449664+sshah3420@users.noreply.github.com>
2023-04-12 21:45:28 -0400

    -

--- DBMS_project/src/getSongByArtistApi.java
+++ DBMS_project/src/getSongByArtistApi.java
@@ -6,37 +6,49 @@ import java.sql.Statement;
 import java.util.Scanner;
 //gets songs by Artist_id
 public class getSongByArtistApi {
-	
-    public public static void getSongsbyArtist(String column, Object value) {
-        try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			
-			// Get Connection object.
-			connection = DriverManager.getConnection(jdbcURL, user, password);
-			System.out.println(connection);
+    private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/sshah34";
+	private static final String user = "sshah34";
+	private static final String password = "200474707";
 
-			
-			String insertSql = " select res.artist_id,Song.song_id,Song.title,Song.album_id,Song.play_count from (select * from Artist_contributes where artist_id = " + value +") res join Song on res.song_id = Song.song_id;";
-			ps = connection.prepareStatement(insertSql);			
-			
-			rs1 = ps.executeQuery();
-			System.out.println("artist_id song_id, title, album_id, play_count");
-			while(rs1.next()) {
-				System.out.print(rs1.getString(1) + " ");
-				System.out.print(rs1.getString(2) + " ");
-				System.out.print(rs1.getString(3) + " ");
-				System.out.print(rs1.getString(4) + " ");
-				System.out.println(rs1.getString(5));
-				
-			}		
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// Close PreparedStatement and Connection Objects.
-			close(ps);
-			close(connection);
-		}
+	public static Connection connection = null;
+	public static Statement stmt = null;
+	public static PreparedStatement ps = null;
+	public static PreparedStatement ps2 = null; 
+	public static ResultSet rs = null;
+	public static ResultSet rs1 = null;
+	
+    public static void getSongsbyArtist(int value) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            // Get Connection object.
+            connection = DriverManager.getConnection(jdbcURL, user, password);
+            System.out.println(connection);
+    
+    
+            String insertSql = " select res.artist_id,Song.song_id,Song.title,Song.album_id,Song.play_count from (select * from Artist_contributes where artist_id = " + value +") res join Song on res.song_id = Song.song_id;";
+            ps = connection.prepareStatement(insertSql);
+    
+            
+            
+            rs1 = ps.executeQuery();
+            System.out.println("artist_id song_id, title, album_id, play_count");
+            while(rs1.next()) {
+                System.out.print(rs1.getString(1) + " ");
+                System.out.print(rs1.getString(2) + " ");
+                System.out.print(rs1.getString(3) + " ");
+                System.out.print(rs1.getString(4) + " ");
+                System.out.println(rs1.getString(5));
+                
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close PreparedStatement and Connection Objects.
+            close(ps);
+            close(connection);
+        }
     }
 
 
